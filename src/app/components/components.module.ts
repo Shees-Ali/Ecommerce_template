f5d19b240b4637ff69eb7824958170875b6bb3a0
huzaifa-qureshi <huzaifa9819891@gmail.com>
2024-05-28 21:24:00 +0500

    Hero-section setup

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HeroSectionComponent],
   imports: [CommonModule, RouterModule, FormsModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, HeroSectionComponent],
 })
 export class ComponentsModule {}
