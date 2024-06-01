52887a86ddb0080fe64890a5078cbf670e91aad5
huzaifa-qureshi <huzaifa9819891@gmail.com>
2024-06-01 14:52:07 +0500

    Hero Section Done

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,9 +1,18 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate(1000, style({ opacity: 1}))
+      ]),
+    ]),
+  ]
 })
 export class HeroSectionComponent {
 
